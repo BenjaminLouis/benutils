82223f0f00c04e84e064be7ee7df351281a078c4
Benjamin Louis <benjaminlouis.pro@gmail.com>
2021-01-13 17:33:40 +0100

    adds common config

--- dev_history.R
+++ dev_history.R
@@ -3,6 +3,8 @@ usethis::use_build_ignore("dev_history.R")
 
 
 ## Fill the DESCRIPTION ----
+## --------------------
+
 # function to do inspired by the golem one
 
 
@@ -19,3 +21,37 @@ usethis::use_git()
 
 # Add a repo and connect to github
 usethis::use_github()
+
+
+## Create Common Files ----
+## -------------------
+
+usethis::use_mit_license( copyright_holder = "Benjamin Louis" )  # You can set another license here
+usethis::use_readme_rmd( open = FALSE )
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+#usethis::use_news_md( open = FALSE )
+
+
+## R files ---
+## -------
+
+#usethis::use_r(")
+
+## Tests ---
+## -----
+
+
+## Dependencies ---
+## ------------
+
+#usethis::use_test(")
+
+# Add to description
+attachment::att_amend_desc(extra.suggests = NULL)
+
+# renv
+renv::init()
+renv::snapshot(type = "explicit")
+renv::restore()
+#renv::rebuild()
